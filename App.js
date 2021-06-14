874b8e981152f17ac77ea907beb446ce4a47c99f
hreyesm <hectorreyesmanrique@gmail.com>
2021-06-13 20:32:46 -0500

    Uncomment lines

--- App.js
+++ App.js
@@ -36,13 +36,13 @@ const App = () => {
       <PaperProvider theme={theme}>
         <NavigationContainer>
           <Stack.Navigator>
-            {/* <Stack.Screen
+            <Stack.Screen
               component={StartScreen}
               name="Bienvenida"
               options={{ headerShown: false }}
             />
             <Stack.Screen component={SignInScreen} name="Inicio de sesión" />
-            <Stack.Screen component={SignUpScreen} name="Registro" /> */}
+            <Stack.Screen component={SignUpScreen} name="Registro" />
             <Stack.Screen
               component={TabNavigator}
               name="App"
